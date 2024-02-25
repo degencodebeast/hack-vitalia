8461ea37ee22812e7daa9a1898a08948a381f55b
Lucky victory <luckyvictory54@gmail.com>
2024-02-25 16:42:13 +0100

    feat: :sparkles: updated footer logo

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -4,11 +4,7 @@ const Footer = () => {
   return (
     <Box as='footer' className='w-full bg-[#014421]' py={8}>
       <div className='max-w-[1300px] w-full px-4 flex justify-between items-center mx-auto'>
-        <Image
-          alt='Logo'
-          src='/images/svg/
-        yellow-logo.svg'
-        />
+        <Image alt='Logo' src='/images/svg/yellow-logo.svg' />
       </div>
     </Box>
   );
